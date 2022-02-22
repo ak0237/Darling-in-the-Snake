66cc42e267f44453af683d3922bb1b27b268d431
andrey <akarv@pop-os.localdomain>
2022-02-22 02:04:46 -0300

    Funcional

--- index.py
+++ index.py
@@ -29,8 +29,8 @@ y_darin = randint(50, 430)
 largura = 640
 altura = 480
 
-x = largura/2
-y = altura/2
+x = int(largura/2)
+y = int(altura/2)
 
 velocidade = 10
 
@@ -54,8 +54,22 @@ def aumenta_z2(lista_z2):
     for XeY in lista_z2:
         pygame.draw.rect(tela, (255,0,255), (XeY[0], XeY[1], 20, 20))
 
+def reinicia_jogo():
+    global pontos, comp_inicial, x, y, lista_cabeca, lista_z2, x_darin, y_darin, murio
+    pontos = 0
+    comp_inicial = 15
+    x = int(largura/2)
+    y = int(altura/2)
+    lista_cabeca = []
+    lista_z2 = []
+    x_darin = randint(40, 600)
+    y_darin = randint(50, 430)
+    murio = False
+
 lista_z2 = []
 comp_inicial = 5
+
+murio = False
 
 ## Loop do Jogo
 while True:
@@ -116,7 +130,6 @@ while True:
         som_colisao.play()
         comp_inicial += 1
         
-        
 
 
     
@@ -124,9 +137,42 @@ while True:
     lista_cabeca.append(x)
     lista_cabeca.append(y)
     lista_z2.append(lista_cabeca)
+
+    if lista_z2.count(lista_cabeca) > 1:
+        fonte2 = pygame.font.SysFont('arial', 20, True, True)
+        mensagem = 'Game Over!!! Pressione R para jogar novamente'
+        texto_formatado = fonte2.render(mensagem, True, (0,0,0))
+        ret_texto = texto_formatado.get_rect()
+
+        murio = True
+        while murio:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_r:
+                        reinicia_jogo()
+
+            tela.fill ((255,255,255))
+
+            ret_texto.center = (largura//2, altura//2)
+            tela.blit(texto_formatado, ret_texto)
+            pygame.display.update()
+
+    if x > largura:
+        x = 0
+    if x < 0:
+        x = largura
+    if y > altura: 
+        y = 0
+    if y < 0: 
+        y = altura
+    
+
     if len(lista_z2) > comp_inicial:
         del lista_z2[0]
-
+    
     aumenta_z2(lista_z2)
 
 
